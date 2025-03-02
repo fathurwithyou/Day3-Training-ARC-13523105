2567cd69e5ff19de9405d7cdd7b1eb31cb2f466f
Ellipsis <rizkyfathur326@gmail.com>
2025-03-03 05:26:48 +0700

    add routes

---
+++ backend/internal/server/server.go
@@ -0,0 +1,14 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/fathurwithyou/Day3-Training-ARC-13523105/backend/internal/handlers"
+	"github.com/fathurwithyou/Day3-Training-ARC-13523105/backend/internal/middleware"
+)
+
+func SetupRoutes(app *fiber.App) {
+	app.Use(middleware.LoggingMiddleware)
+
+	app.Get("/users", handlers.GetUsers)
+	app.Post("/users", handlers.CreateUser)
+}
